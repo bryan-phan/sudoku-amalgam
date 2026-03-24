b7cfc16ee02e9b84153ea5769fa8a5ba742c526e
Bryan <phanbryan@hotmail.com>
2026-03-24 00:27:11 -0500

    warped image to fit screen and spliced into 81 cells

--- cv/new.py
+++ cv/new.py
@@ -1,12 +1,18 @@
 import cv2
 import os
+import numpy as np
 
 class Scan:
     def __init__(self):
         self.path = os.getcwd() + r"\cv" + r"\test_imgs" + r"\angled.jpg"
 
-        self.img = cv2.imread(self.path)
-        self.img = cv2.resize(self.img, None, fx=0.5, fy=0.4)
+        #apparently i need to store the orignal for later and make a copy of it for now
+        self.original = cv2.imread(self.path)
+        self.original = cv2.resize(self.original, None, fx=0.5, fy=0.4)
+
+        self.img = self.original.copy()
+        #for da 4 corner contour used to warp image to straight
+        self.biggest = None
 
         self.grayscale()
         self.blur()
@@ -37,22 +43,89 @@ class Scan:
 
         max_area = 0
         biggest = None
+        #find the biggest contour since the image mixed hella contours together
 
         for cnt in contours:
             area = cv2.contourArea(cnt)
-            if area > max_area:
+            perimeter = cv2.arcLength(cnt, True)
+
+            #approximates the shape with verticies
+            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+
+            if area > max_area and len(approx) == 4:
                 max_area = area
-                biggest = cnt
+                biggest = approx
 
-        cv2.drawContours(self.img, [biggest], -1, (0, 255, 0), 2)
+        self.biggest = biggest
 
+        if biggest is not None:
+            cv2.drawContours(self.img, [biggest], -1, (0, 255, 0), 2)
+            self.img = self.warp(biggest)
+
+    def order_points(self, points):
+        points = points.reshape(4, 2).astype(np.float32)
+        ordered = np.zeros((4, 2), dtype=np.float32)
+
+        sums = points.sum(axis=1)
+        diffs = np.diff(points, axis=1)
+
+        ordered[0] = points[np.argmin(sums)]   # top-left
+        ordered[2] = points[np.argmax(sums)]   # bottom-right
+        ordered[1] = points[np.argmin(diffs)]  # top-right
+        ordered[3] = points[np.argmax(diffs)]  # bottom-left
+
+        return ordered
+
+    def warp(self, biggest):
+        src = self.order_points(biggest)
+
+        width_top = np.linalg.norm(src[1] - src[0])
+        width_bottom = np.linalg.norm(src[2] - src[3])
+        height_left = np.linalg.norm(src[3] - src[0])
+        height_right = np.linalg.norm(src[2] - src[1])
+
+        width = int(max(width_top, width_bottom))
+        height = int(max(height_left, height_right))
+
+        dst = np.array([
+            [0, 0],
+            [width - 1, 0],
+            [width - 1, height - 1],
+            [0, height - 1]
+        ], dtype=np.float32)
+
+        matrix = cv2.getPerspectiveTransform(src, dst)
+        return cv2.warpPerspective(self.original, matrix, (width, height))
     
-    #def warp(self, biggest):
+    def splice(self):
+        height, width = self.original.shape[:2]
+
+        #divides the image into a 9x9 grid
+        cell_h = height // 9
+        cell_w = width // 9
+
+        cells = []
+
+        for r in range(9):
+            row_cells = []
+            for c in range(9):
+                y1 = r * cell_h
+                y2 = (r + 1) * cell_h
+                x1 = c * cell_w
+                x2 = (c + 1) * cell_w
+
+                cell = self.original[y1:y2, x1:x2]
+                #inside row_cells are column cells
+                row_cells.append(cell)
+            #makes the entire grid
+            cells.append(row_cells)
+
+        return cells
+            
 
 
 
 
-    
 
 
 
